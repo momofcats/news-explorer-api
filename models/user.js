ead25d6d0f056eb8d4b9e07ca0b0f9bc5e268c61
Yuliya Khilko <ykhilko@gmail.com>
2020-11-27 10:08:54 -0800

    add article and user schema

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
+
+const userSchema = new mongoose.Shema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Wrong email format',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+}, { versionKey: false });
+
+module.exports = mongoose.module('user', userSchema);
