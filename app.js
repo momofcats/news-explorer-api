b80d16f98e07b8a99a15606a529ea70e1d2c4e75
Yuliya Khilko <ykhilko@gmail.com>
2020-11-26 17:21:45 -0800

    add user and article schemas and models

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/newsdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
