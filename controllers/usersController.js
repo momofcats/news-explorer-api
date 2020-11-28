3e6b250212a018c963371850b98d87eefd6d5c24
Yuliya Khilko <ykhilko@gmail.com>
2020-11-27 18:37:53 -0800

    update usercontriller with centralized error handler

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,12 +1,23 @@
 const User = require('../models/user');
 
-const getUsers = (req, res) => {
+const {
+  STATUS_CODE_OK,
+} = require('../utils/statusCodes');
+
+const NotFoundError = require('../errors/not-found-err');
+
+const getUsers = (req, res, next) => {
   User.find({})
-    .then((users) => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'error' }));
+    .then((users) => {
+      if (users.length === 0) {
+        throw new NotFoundError('Users were not found');
+      }
+      return res.status(STATUS_CODE_OK).send(users);
+    })
+    .catch(next);
 };
 
-const getUserInfo = (req, res) => {
+const getUserInfo = (req, res, next) => {
   let userId = req.params.id;
   if (userId === 'me') {
     userId = req.user._id;
@@ -14,11 +25,11 @@ const getUserInfo = (req, res) => {
   User.findById(userId)
     .then((user) => {
       if (!user) {
-        return res.status(400).send({ message: 'No user with matching ID found' });
+        throw new NotFoundError('No user with matching ID found');
       }
-      return res.status(200).send(user);
+      return res.status(STATUS_CODE_OK).send(user);
     })
-    .catch();
+    .catch(next);
 };
 
 module.exports = {
