ec7e151b685fd8a02da4ef06017a3e4f59bcb399
Yuliya Khilko <ykhilko@gmail.com>
2020-12-02 16:41:55 -0800

    update routes

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
-
+const limiter = require('./utils/rateLimit');
 require('dotenv').config();
 const mainRouter = require('./routes/index');
 
@@ -12,26 +11,27 @@ const errorHandler = require('./middleware/errorHandler');
 
 const jsonParser = bodyParser.json();
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DATABASE_URL, NODE_ENV } = process.env;
 
 const app = express();
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-});
 
-mongoose.connect('mongodb://localhost:27017/newsdb', {
+mongoose.connect(NODE_ENV === 'production' ? DATABASE_URL : 'mongodb://localhost:27017/newsdb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
+
 app.use(limiter);
 app.use(helmet());
 app.use(jsonParser);
 app.use(requestLogger);
 app.use('/', mainRouter);
+app.use((req, res) => {
+  res.status(404).send({ message: 'Requested resource not found' });
+});
 app.use(errorLogger);
 app.use(errorHandler);
+
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
 });
