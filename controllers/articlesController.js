19093779037ed7bd3e2ae2386e95428d4a16426c
Yuliya Khilko <ykhilko@gmail.com>
2021-01-12 11:43:29 -0800

    add owner field when delete articles

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -34,7 +34,7 @@ const createArticle = (req, res, next) => {
 };
 
 const deleteArticle = (req, res, next) => {
-  Article.findById(req.params.articleId)
+  Article.findById(req.params.articleId).select('+owner')
     .then((article) => {
       if (!article) {
         throw new NotFoundError(errorMessages.noMatchingArticle);
