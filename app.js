4058a7e13c06ba1aaed61c9e643d1e35588cabce
Yuliya Khilko <ykhilko@gmail.com>
2021-01-21 21:32:34 -0800

    disable cors

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ mongoose.connect(NODE_ENV === 'production' ? DATABASE_URL : 'mongodb://localhost
   useCreateIndex: true,
   useFindAndModify: false,
 });
-app.use(cors());
+// app.use(cors());
 app.options('*', cors());
 app.use(limiter);
 app.use(helmet());
